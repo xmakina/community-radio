1bd14624c07b89832b2b9f469bde1a6667530cc8
Simon Staton <simon.staton@live.co.uk>
2016-01-08 15:04:13 +0000

    Adding commented live db details

--- app/database.js
+++ app/database.js
@@ -2,3 +2,6 @@ module.exports = {
 	//url: 'mongodb://172.16.120.72/twogether-radio'
 	url: (process.env.DATABASE || 'mongodb://localhost/twogether-radio')
 }
+
+// mongodb://admin:dJwDkWWjea@ds039125.mongolab.com:39125/community-radio-dev
+// mongodb://admin:dJwDkWWjea@ds039175.mongolab.com:39175/community-radio-prod
